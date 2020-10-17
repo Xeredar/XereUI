02e78e18420089a546c233a5231aa4d828a24a97
Xeredar <k.vonwendt@gmail.com>
2020-10-17 02:45:11 +0200

    Removed profiles as they lost their meaning without the raid profiles. Also removed some other customization options. The addon now only repositions and resizes the basic ui elements

--- core.lua
+++ core.lua
@@ -5,13 +5,7 @@
 
 ]]
 
-local defaultDB = {};
--- Do nothing to this value! Ever! I mean it!
-defaultDB[1] = 1.2;
--- Sets the default UI profile
-defaultDB[2] = "normal";
--- Sets, whether nameplates should be small or big on fitting resolutions
-defaultDB[3] = true;
+local version = 1.0;
 
 -- Determines you current resolution to be lowRes or not
 -- Seperate "local global variable" so that multiple setups can use it
@@ -34,13 +28,6 @@ function setCVars()
 	SetCVar("nameplateShowSelf", 0, "scriptCVar");
 	SetCVar("colorChatNamesByClass", 1, "scriptCVar");
 	SetCVar("ShowClassColorInNameplate", 1, "scriptCVar");
-	SetCVar("NamePlateVerticalScale", 2.7, "scriptCVar");
-	SetCVar("NamePlateHorizontalScale", 1.4, "scriptCVar");
-
-	if (((XereUICharDB[2] == "normal") or (XereUICharDB[2] == "healer")) and XereUICharDB[4]) then
-		SetCVar("NamePlateVerticalScale", 1, "scriptCVar");
-		SetCVar("NamePlateHorizontalScale", 1, "scriptCVar");
-	end
 end
 
 
@@ -54,53 +41,23 @@ end
 function setPositions ()
 	TARGET_FRAME_BUFFS_ON_TOP = true;
 	FOCUS_FRAME_BUFFS_ON_TOP = true;
-	if ((XereUICharDB[2] == "normal") and (not lowRes)) then
+	if not lowRes then
 		PLAYER_FRAME_CASTBARS_SHOWN = false;
 	else
 		PLAYER_FRAME_CASTBARS_SHOWN = true;
 	end
 	local scale, playerX, playerY;
-	if (XereUICharDB[2] == "normal") then
-		if (lowRes) then
-			scale = 1.1;
-			playerX = -200;
-			playerY = -50;
-			positionFrames(1, (scale - 0.1), playerX, playerY, (playerX * (-1)), playerY, (playerX - 100), (playerY + 100));
-		else			
-			scale = 1.3;
-			playerX = -250;
-			playerY = -50;
-			positionFrames(scale, (scale - 0.2), playerX, playerY, (playerX * (-1)), playerY, (playerX - 100), (playerY + 150));
-		end
-		
-	elseif (XereUICharDB[2] == "pvp") then
-		if (lowRes) then
-			scale = 1;
-			playerX = -400;
-			playerY = 50;
-			positionFrames(scale, scale, playerX, playerY, (playerX + 230), playerY, (playerX + 20), (playerY + 100));
-		else
-			scale = 1.2;
-			playerX = -400;
-			playerY = 50;
-			positionFrames(scale, scale, playerX, playerY, (playerX + 230), playerY, (playerX - 50), (playerY + 100));
-		end
-		
-	elseif (XereUICharDB[2] == "healer") then
-		if (lowRes) then
-			scale = 1.1;
-			playerX = -200;
-			playerY = -50;
-			positionFrames(scale, (scale - 0.1), playerX, playerY, (playerX * (-1)), playerY, (playerX - 100), (playerY + 100));
-		else
-			scale = 1.3;
-			playerX = -250;
-			playerY = -50;
-			positionFrames(scale, (scale - 0.2), playerX, playerY, (playerX * (-1)), playerY, (playerX + 20), (playerY + 150));
-		end
-		
-	else
-		print("Chosen profile not recognized.");
+
+	if (lowRes) then
+		scale = 1.1;
+		playerX = -200;
+		playerY = -50;
+		positionFrames(1, (scale - 0.1), playerX, playerY, (playerX * (-1)), playerY, (playerX - 100), (playerY + 100));
+	else			
+		scale = 1.3;
+		playerX = -250;
+		playerY = -50;
+		positionFrames(scale, (scale - 0.2), playerX, playerY, (playerX * (-1)), playerY, (playerX - 100), (playerY + 150));
 	end
 end
 
@@ -154,6 +111,10 @@ Minimap:SetScript("OnMouseUp", function(self, button)
 	end
 end);
 
+-- Moves the tooltip to be anchored at the cursos
+
+hooksecurefunc("GameTooltip_SetDefaultAnchor", function(self, parent) self:SetOwner(parent, "ANCHOR_CURSOR") end)
+
 
 --[[
 
@@ -167,28 +128,7 @@ frame:RegisterEvent("PLAYER_ENTERING_WORLD");
 frame:RegisterEvent("CVAR_UPDATE"); -- needed so that CVars can be set in a script
 
 local function eventHandler(self, event, ...)
-	if (event == "ADDON_LOADED") then
-	
-		-- Checks whether the addon has run before
-		if (XereUICharDB == nil) then
-			XereUICharDB = defaultDB;
-			print("|cff9482C9XereUI|r |cff34d1c9v." .. XereUICharDB[1] .. "|r Setup done.");
-			
-		-- Checks whether the saved variables are out of date
-		elseif (XereUICharDB[1] ~= defaultDB[1]) then
-			local tempData = XereUICharDB;
-
-			XereUICharDB = defaultDB;
-
-			-- Copies the old values to the new database
-			for i=2, tablelength(tempData) do
-				XereUICharDB[i] = tempData[i];
-			end
-
-			print("|cff9482C9XereUI|r | updated to |cff34d1c9v." .. XereUICharDB[1] "|r. Have fun!");
-		end
-		
-	elseif (event == "PLAYER_ENTERING_WORLD") then	
+	if (event == "PLAYER_ENTERING_WORLD") then	
 		-- Applies the profile to the interface whenever the player enters the world
 		setPositions();
 		setCVars();
@@ -205,50 +145,9 @@ frame:SetScript("OnEvent", eventHandler);
 ]]
 
 local function MyAddonCommands(command, editbox)
-
 	-- Changes the profile to the specified one
-	if (command == "normal") or (command == "pvp") or (command == "healer") then
-		XereUICharDB[2] = command;
-		setCVars();
-		ReloadUI();
-
-	-- toggles the nameplate size
-	elseif (command == "nameplate") then
-		XereUICharDB[3] = not XereUICharDB[3];
-		ReloadUI();
-		
-	-- prints a list of the savedvariables and their values
-	elseif (command == "status") then
-	
-		print("Version: " .. XereUICharDB[1]);
-		print("Profile: " .. XereUICharDB[2]);
-
-		if (XereUICharDB[3]) then
-			print("Nameplate autosize: true");
-		else
-			print("Nameplate autosize: false");
-		end
-	
-	-- resets all saved variables to their standard values
-	elseif (command == "reset") then
-		XereUICharDB = nil;
-		ReloadUI();
-		
-	-- prints the help message
-	elseif (command == "help") then
-		print("====================================================");
-		print("Use |cff9482C9/xui|r or |cff9482C9/xereui|r with one of the following commands:");
-		print("    > |cff34d1c9normal|r : for the standard UI.");
-		print("    > |cff34d1c9pvp|r : for the pvp focused UI.");
-		print("    > |cff34d1c9healer|r : for a healing centric UI.");
-		print(" ");
-		print("    > |cff34d1c9nameplate|r : toggles small/big nameplates for fitting resolutions.");
-		print(" ");
-		print("    > |cff34d1c9help|r : prints this help.");
-		print("    > |cff34d1c9status|r : displays your current saved values.");
-		print("    > |cff34d1c9reset|r : resets all saved variables to their standard values.");
-		print("====================================================");
-		
+	if (command == "status") or (command == "help") then
+		print("Version: " .. version);
 	else
 		print("Command not recognized. Try '/xui help' for help.");
 	end
